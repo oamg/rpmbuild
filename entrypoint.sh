ee14574d410e23e2c1eab79fd7ab526e1699d9ba
Glen Johnson <gsfjohnson@gmail.com>
2020-05-30 20:51:28 -0700

    improve output

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ fx_cmd () {
   "$@"
   ERR=$?
   if [ $ERR -gt 0 ]; then
-    echo ::error::command returned non-zero exitcode ${ERR}
+    echo ::error::$@ failed ${ERR}
     exit ${ERR}
   fi
   echo ::endgroup::$@
@@ -29,7 +29,7 @@ fi
 fx_cmd rpmdev-setuptree
 
 # Copy spec file from path specPath to /root/rpmbuild/SPECS/
-fx_cmd cp /github/workspace/${specPath} /github/home/rpmbuild/SPECS/
+fx_cmd cp -v /github/workspace/${specPath} /github/home/rpmbuild/SPECS/
 
 # Rewrite Source: key in spec file
 sed -i "s=Source:.*=Source: %{name}-%{version}.tar.gz=" /github/home/rpmbuild/SPECS/${specFile}
@@ -38,19 +38,19 @@ sed -i "s=Source:.*=Source: %{name}-%{version}.tar.gz=" /github/home/rpmbuild/SP
 fx_cmd curl --location --output tmp.tar.gz https://api.github.com/repos/${GITHUB_REPOSITORY}/tarball/${GITHUB_REF}
 
 # create directory to match source file - %{name}-{version}.tar.gz of spec file
-fx_cmd mkdir ${name}-${version}
+fx_cmd mkdir -v ${name}-${version}
 
 # Extract source code 
 fx_cmd tar xf tmp.tar.gz -C ${name}-${version} --strip-components 1
 
 # Create Source tar.gz file 
-fx_cmd tar -czf ${name}-${version}.tar.gz ${name}-${version}
+fx_cmd tar czf ${name}-${version}.tar.gz ${name}-${version}
 
 # list files in current directory /github/workspace/
 # await gha_exec('ls -la ');
 
 # Copy tar.gz file to source path
-fx_cmd cp ${name}-${version}.tar.gz /github/home/rpmbuild/SOURCES/
+fx_cmd cp -v ${name}-${version}.tar.gz /github/home/rpmbuild/SOURCES/
 
 # install all BuildRequires: listed in specFile
 fx_cmd yum-builddep --assumeyes /github/home/rpmbuild/SPECS/${specFile}
@@ -58,29 +58,28 @@ fx_cmd yum-builddep --assumeyes /github/home/rpmbuild/SPECS/${specFile}
 # main operation
 fx_cmd rpmbuild -ba /github/home/rpmbuild/SPECS/${specFile}
 
-# Verify RPM is created
-fx_cmd ls /github/home/rpmbuild/RPMS
+# Verify binary output
+fx_cmd find /github/home/rpmbuild/RPMS
 
 # setOutput rpm_path to /root/rpmbuild/RPMS , to be consumed by other actions like 
 # actions/upload-release-asset 
 
 # Get source rpm name , to provide file name, path as output
-myOutput=''
-fx_cmd ls /github/home/rpmbuild/SRPMS/
+SRPM=$(ls -1 /github/home/rpmbuild/SRPMS/ | grep ${name})
 
 # only contents of workspace can be changed by actions and used by subsequent actions 
 # So copy all generated rpms into workspace , and publish output path relative to workspace (/github/workspace)
-fx_cmd mkdir -p rpmbuild/SRPMS
-fx_cmd mkdir -p rpmbuild/RPMS
+fx_cmd mkdir -vp rpmbuild/SRPMS
+fx_cmd mkdir -vp rpmbuild/RPMS
 
-fx_cmd cp /github/home/rpmbuild/SRPMS/${myOutput} rpmbuild/SRPMS
-fx_cmd cp -R /github/home/rpmbuild/RPMS/. rpmbuild/RPMS/
+fx_cmd cp -v $(find -type f /github/home/rpmbuild/SRPMS) rpmbuild/SRPMS/
+fx_cmd cp -v $(find -type f /github/home/rpmbuild/RPMS) rpmbuild/RPMS/
 
 fx_cmd ls -la rpmbuild/SRPMS
 fx_cmd ls -la rpmbuild/RPMS
 
-echo "::set-output source_rpm_dir_path=rpmbuild/SRPMS/"
-echo "::set-output source_rpm_path=rpmbuild/SRPMS/${myOutput}"
-echo "::set-output source_rpm_name=${myOutput}"
-echo "::set-output rpm_dir_path=rpmbuild/RPMS/"
+echo "::set-output srpm_dir=rpmbuild/SRPMS/"
+echo "::set-output srpm_path=rpmbuild/SRPMS/${SRPM}"
+echo "::set-output srpm_name=${SRPM}"
+echo "::set-output rpm_dir=rpmbuild/RPMS/"
 echo "::set-output rpm_content_type=application/octet-stream"
